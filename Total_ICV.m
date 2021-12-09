46912e70cb216e59817aca669c051b13000b6c39
Jan Axelsson <axelsson.jan@gmail.com>
2021-12-09 15:03:31 +0100

    Jan test

--- Total_ICV.m
+++ Total_ICV.m
@@ -4,4 +4,4 @@
 %Step 3: Scripts/ SPM/ SPM tissue propabilaty map ROIs
 %Step 4: Export to workspace
 %Step 5: run this script
-imlook4d_ROI = and( (imlook4d_ROI < 4) , (imlook4d_ROI > 0) ); Import
+imlook4d_ROI = and( (imlook4d_ROI < 4) , (imlook4d_ROI > 0) ); Import %
