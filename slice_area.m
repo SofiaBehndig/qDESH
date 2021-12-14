a1accb50b95d1d5f6b6e90f45d0ec83b023c4e2b
Jan Axelsson <axelsson.jan@gmail.com>
2021-12-14 18:40:01 +0100

    New fix for wrong slice_area

--- slice_area.m
+++ slice_area.m
@@ -12,33 +12,36 @@
 
     IGNORE_LAST_ROI = true;  % true = Ignore last ROI, which is Ventricle volume, false = all ROIs
 
-
+    
+    % Initiate
     StoreVariables
     SelectOrientation('Ax');
     Export
     
     TAB=sprintf('\t');
     clipboardArea = '';
+    area = [];
+    dA = 0.01 * imlook4d_current_handles.image.pixelSizeX * imlook4d_current_handles.image.sliceSpacing; % cm2
     
     numberOfRois = length(imlook4d_ROINames) - 1;
     
+    
+    % Number of ROIs
     numberOfOutputs = numberOfRois;             % Alt 1) Use all ROIs
     if (IGNORE_LAST_ROI)
         numberOfOutputs = numberOfRois - 2;     % Alt 2) Ignore last two ROIs
     end
     
+    
+    % Calculate areas
     for i = 1 : numberOfOutputs
+       ySlicePos(i) = min( find(  sum( ( imlook4d_ROI == i) , [1,3] ) > 0 ) );
+       area(i) = sum( squeeze( imlook4d_ROI( :, ySlicePos(i), :) == i ), 'all')  * dA;  % Sum pixels in current ROI, for slice at y=ySlicePos
 
-        ySlicePos(i) = min( find(  sum( ( imlook4d_ROI == i) , [1,3] ) > 0 ) );
-        numberOfPixelsArea = sum( squeeze( imlook4d_ROI( :, ySlicePos, :) == i ), 'all'); % Sum pixels in current ROI, for slice at y=ySlicePos
-        dA = 0.01 * imlook4d_current_handles.image.pixelSizeX * imlook4d_current_handles.image.sliceSpacing; % cm2
-        area(i) = numberOfPixelsArea * dA;
-        
-        %disp( [ imlook4d_ROINames{i} TAB num2str(area(i)) ' mm2' TAB '(in y = ' num2str(ySlicePos(i)) ')' ] );
-        
-        clipboardArea = [clipboardArea num2str( area(i)) TAB];
+       clipboardArea = [clipboardArea num2str( area(i) ) TAB];
     end
     
+
     % Display
     rowNames = imlook4d_ROINames(1:numberOfOutputs);
     t = table( area', ySlicePos', ...
@@ -46,9 +49,13 @@
         'RowNames', rowNames );
     
     disp(t)
+
+    
+    % Clipboard
+    clipboard('copy', clipboardArea(1:end-1));
     disp('area is copied to clipboard');
 
-    clipboard('copy', clipboardArea(1:end-1))
-
+    
+    % Finish
     ClearVariables
  
