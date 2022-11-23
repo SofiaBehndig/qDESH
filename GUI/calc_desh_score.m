acb6f0ce05dcd1d0c4536afd7d569e0e19080560
Jan Axelsson <axelsson.jan@gmail.com>
2022-11-23 11:37:05 +0100

    Comment Q-DESH calculations

--- GUI/calc_desh_score.m
+++ GUI/calc_desh_score.m
@@ -9,16 +9,26 @@
 % -----------
 returnCode = 1; % Assume error, correct at end
 
+% Volumes from ROIs
 SYLV_L = imlook4d_ROI_data.volume(1);
 SYLV_R = imlook4d_ROI_data.volume(2);
 TOPHAT = imlook4d_ROI_data.volume(3);
-REF_VOLUME = Vgoal;  % From top_hat_shrink
 
-desh_value = ( SYLV_L + SYLV_R ) / ( TOPHAT * REF_VOLUME / Vtophat0 );  % Vtophat0 is what we found
+% Reference goal volume, and actual start volume
+REF_VOLUME = Vgoal;             % Defined in top_hat_shrink
+ACTUAL_START_VOLUME = Vtophat0; % Found in top_hat_shrink
+
+% Q-DESH is ratio :  [ Sylvian-fissure-volume ] / [ Tophat volume ]
+% where 
+% [ Tophat volume ] = TOPHAT * (REF_VOLUME / ACTUAL_START_VOLUME) 
+% which is normalized by factor  (REF_VOLUME / ACTUAL_START_VOLUME)  to correct for variation in actual start volume
+%
+% Q-DESH :
+desh_value = ( SYLV_L + SYLV_R ) / (  TOPHAT * (REF_VOLUME / ACTUAL_START_VOLUME)  );  
 
 %Import
 
 % Clean up from GUI
-clear SYLV_L SYLV_R TOPHAT REF_VOLUME
+clear SYLV_L SYLV_R TOPHAT REF_VOLUME ACTUAL_START_VOLUME
 
 returnCode = 0; % If it comes here a zero is returned
